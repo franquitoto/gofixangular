9983b2d9db13eab4ba86a873f16ad3cdcbf3845c
ARIAS <farias@laserargentina.local>
2023-09-14 17:01:09 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { SloganComponent } from './slogan/slogan.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    BannerComponent,
+    SloganComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
